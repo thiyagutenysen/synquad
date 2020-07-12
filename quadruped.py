b7d5ac7790a1aed52e5dee27a6d59ca31501ec13
sashank tirumala <stsashank6@gmail.com>
2020-07-12 09:54:55 +0530

    Added functionality to store robot data as json and later load that data

---
+++ quadruped.py
@@ -0,0 +1,15 @@
+import sys, os
+import json
+class QuadrupedRobot():
+    def __init__(self, data):
+        self.name = data['name']
+        self.urdf_path = os.path.realpath('')+ data['urdf_path']
+        self.init_pos = data['init_pos']
+        self.init_ori = data['init_ori']
+        pass
+
+if(__name__ == "__main__"):
+    with open("stoch.json") as f:
+        data = json.load(f)
+    stoch = QuadrupedRobot(data)
+    print(stoch.init_pos)
