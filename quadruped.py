d7b7ba0afdb0ad3b65f7d21ff14174743cfc1d19
sashank tirumala <stsashank6@gmail.com>
2020-07-13 16:29:21 +0530

    Added code that enables pd control of quadruped robot

--- quadruped.py
+++ quadruped.py
@@ -1,17 +1,31 @@
 import sys, os
 import json
+from simulation import SimulationInterface
+from actuator import Default
 class QuadrupedRobot():
+    """
+    The goal of this class is to simply store data pertaining to the quadruped robot.
+    """
     def __init__(self, data):
         self.name = data['name']
         self.urdf_path = os.path.realpath('')+ data['urdf_path']
         self.init_pos = data['init_pos']
         self.init_ori = data['init_ori']
-        self.id = 0
-        self.pyb=0
+        self.sim = SimulationInterface(data)
+        if(data['motor_model']['type'] == "Default"):
+            self.motor = Default(data['motor_model'])
         pass
+    
+    def apply_pd_control(self, des_pos, des_vel):
+        current_ang = self.sim.get_motor_angles()
+        current_vel = self.sim.get_motor_vel()
+        motor_torque = self.motor.calc_torque(des_pos, des_vel, current_ang, current_vel)
+        self.sim.apply_torque(motor_torque)
+        pass
+    
 
 if(__name__ == "__main__"):
-    with open("laikago.json") as f:
+    with open("robots/stoch_two_abduction_urdf/stoch.json") as f:
         data = json.load(f)
     stoch = QuadrupedRobot(data)
-    print(stoch.init_pos)
+    print(stoch.motor._kd)
